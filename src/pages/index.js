554ea0b98dc2dec1d38a07f6c16aeed991332e69
John Wilson <johnbarrierwilson@gmail.com>
2018-11-01 15:09:55 -0400

    Move sections from pages to a new sections folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import Layout from '../components/layout'
 import theme from '../theme'
 
-import Fellows from'./teaching-fellows'
-import FellowSinclair from './fellow-sinclair'
-import Introduction from './introduction'
-import IntroductionRead from './introduction-read'
-import PresidentLetter from './president-letter'
-import Read01 from './read-01'
-import StoryXavier from './story-xavier'
+import Fellows from'../sections/teaching-fellows'
+import FellowSinclair from '../sections/fellow-sinclair'
+import Introduction from '../sections/introduction'
+import IntroductionRead from '../sections/introduction-read'
+import PresidentLetter from '../sections/president-letter'
+import Read01 from '../sections/read-01'
+import StoryXavier from '../sections/story-xavier'
 
 const IndexPage = () => (
   <ThemeProvider theme={theme} >

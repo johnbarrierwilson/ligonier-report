abdd6ec27e9458ac6f688148e4edb11df3bf773e
John Wilson <johnbarrierwilson@gmail.com>
2018-11-03 10:09:48 -0400

    Set foundation for responsive work

--- src/components/grid.js
+++ src/components/grid.js
@@ -2,16 +2,18 @@ import styled from 'styled-components'
 
 const Grid = styled('div')`
   background: ${p => p.theme.colors.white};
-  display: grid;
-  grid-template-columns: 40px repeat(12, 1fr) 40px;
+  /* grid-template-columns: 40px repeat(12, 1fr) 40px; */
+  padding: 0 40px;
   position: relative;
   transition: transform 500ms cubic-bezier(.55,0,.1,1), opacity 500ms cubic-bezier(.55,0,.1,1);
   z-index: ${p => p.theme.index.grid};
   @media (min-width: ${p => p.theme.breakpoints.medium}) {
+    display: grid;
     grid-template-columns: 90px repeat(12, 1fr) 90px;
     grid-template-rows: 90px repeat(11, 1fr);
     height: 100vh;
     overflow: hidden;
+    padding: 0;
   }
 `
 

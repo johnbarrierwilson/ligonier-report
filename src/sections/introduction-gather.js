e93659eba68e5446b21d54fec2e73cc657c57afc
John Wilson <johnbarrierwilson@gmail.com>
2018-11-08 12:37:52 -0500

    Re-organize section intros

--- src/sections/introduction-gather.js
+++ src/sections/introduction-gather.js
@@ -13,13 +13,7 @@ import IntroductionGather from '../images/IntroductionGather.png'
 const IndexPage = () => (
   <Grid name="introductiongather">
     <Backdrop />
-    <Heading
-      column={['3 / span 4', '3 / span 4', '3 / span 4', '3 / span 4']}
-      row={['4 / span 1', '4 / span 1', '4 / span 1', '4 / span 1']}
-      size="xl"
-      text="Gather"
-    />
-    <Well size="xl">
+    <Well size="l">
       <Image
         column={['9 / span 4', '9 / span 4', '9 / span 4', '9 / span 4']}
         row={['4 / span 6', '4 / span 6', '4 / span 6', '4 / span 6']}
@@ -27,13 +21,21 @@ const IndexPage = () => (
         src={IntroductionGather}
       />
     </Well>
-    <Content
-      column={['3 / span 4', '3 / span 4', '3 / span 4', '3 / span 4']}
-      row={['8 / span 3', '8 / span 3', '8 / span 3', '8 / span 3']}
-      type="deck"
-    >
-      <p><strong>Iron sharpens iron.</strong> Every year, in cities around the world and online, Ligonier Ministries gathers Christians at events to study God’s Word for mutual edification. It’s a privilege to meet, serve, and learn from God’s people. Our aim is to bless the local church and strengthen its members for greater service. Thank you for joining&nbsp;us.</p>
-    </Content>
+    <Well size="l">
+      <Heading
+        column={['3 / span 4', '3 / span 4', '3 / span 4', '3 / span 4']}
+        row={['4 / span 1', '4 / span 1', '4 / span 1', '4 / span 1']}
+        size="xl"
+        text="Gather"
+      />
+      <Content
+        column={['3 / span 4', '3 / span 4', '3 / span 4', '3 / span 4']}
+        row={['8 / span 3', '8 / span 3', '8 / span 3', '8 / span 3']}
+        type="deck"
+      >
+        <p><strong>Iron sharpens iron.</strong> Every year, in cities around the world and online, Ligonier Ministries gathers Christians at events to study God’s Word for mutual edification. It’s a privilege to meet, serve, and learn from God’s people. Our aim is to bless the local church and strengthen its members for greater service. Thank you for joining&nbsp;us.</p>
+      </Content>
+    </Well>
   </Grid>
 )
 

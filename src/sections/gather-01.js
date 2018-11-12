9e139b27d6b1f0c2c9cff4cdd3d97cb1f8fd80e7
John Wilson <johnbarrierwilson@gmail.com>
2018-11-12 18:51:28 -0500

    Fix gather images to use gatsby-image

--- src/sections/gather-01.js
+++ src/sections/gather-01.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
 import Content from '../components/content'
@@ -10,89 +11,108 @@ import NumbersLarge from '../components/numbersLarge'
 import Subject from '../components/subject'
 import Well from '../components/well'
 
-import Seattle from '../images/ConferenceSeattle.jpg'
-import Conference from '../images/Conference.jpg'
-
 const IndexPage = () => (
-  <Grid name="gather01">
-    <Well bottom={true}>
-      <Image
-        column={['2 / span 7', '2 / span 7', '2 / span 7', '2 / span 7']}
-        position='center bottom'
-        row={['1 / span 6', '1 / span 6', '1 / span 6', '1 / span 6']}
-        src={Conference}
-      />
-    </Well>
-    <Subject
-      column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
-      row={['7 / span 1', '7 / span 1', '7 / span 1', '7 / span 1']}
-    >
-      National Conference
-    </Subject>
-    <Divider
-      column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
-      row={['7 / span 2', '7 / span 2', '7 / span 2', '7 / span 2']}
-    />
-    <Heading
-      center={true}
-      column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
-      row={['8 / span 2', '8 / span 2', '8 / span 2', '8 / span 2']}
-      text="GATHERING FOR AN ANNUAL FAMILY REUNION"
-    />
-    <Content
-      center={true}
-      column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
-      row={['10 / span 2', '10 / span 2', '10 / span 2', '10 / span 2']}
-    >
-      <p>For over thirty years, our National Conference has been gathering thousands of Ligonier friends and supporters to hear trustworthy teaching from God’s Word. The conference is held over three days every spring in Orlando, Fla., and brings together Christians from many different backgrounds who make up the Ligonier family. Each year it is encouraging to see more firsttime attendees gathering from around the world. Your prayers and support enable our National Conference to serve as a great platform for equipping Christians with the truth.</p>
-    </Content>
-
-    <Well size="l" top={true}>
-      <Backdrop />
-      <Subject
-        column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-        row={['1 / span 2', '1 / span 2', '1 / span 2', '1 / span 2']}
-      >
-        Regional &amp; International Conferences
-      </Subject>
-      <Divider
-        column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-        row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
-      />
-      <Heading
-        center={true}
-        column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-        row={['2 / span 3', '2 / span 3', '2 / span 3', '2 / span 3']}
-        text="GATHERING LOCALLY AND AROUND THE WORLD"
-      />
-      <Image
-        column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-        position='left center'
-        row={['4 / span 3', '4 / span 3', '4 / span 3', '4 / span 3']}
-        size='contain'
-        src={Seattle}
-        transform='translateY(1.5rem)'
-      />
-      <Well>
+  <StaticQuery
+    query={graphql`
+      query Gather01Query {
+        Conference: file(relativePath: { eq: "Conference.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1500) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        ConferenceSeattle: file(relativePath: { eq: "ConferenceSeattle.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <Grid name="gather01">
+        <Well bottom={true}>
+          <Image
+            column={['2 / span 7', '2 / span 7', '2 / span 7', '2 / span 7']}
+            position='center bottom'
+            row={['1 / span 6', '1 / span 6', '1 / span 6', '1 / span 6']}
+            src={data.Conference.childImageSharp.fluid}
+          />
+        </Well>
+        <Subject
+          column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
+          row={['7 / span 1', '7 / span 1', '7 / span 1', '7 / span 1']}
+        >
+          National Conference
+        </Subject>
+        <Divider
+          column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
+          row={['7 / span 2', '7 / span 2', '7 / span 2', '7 / span 2']}
+        />
+        <Heading
+          center={true}
+          column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
+          row={['8 / span 2', '8 / span 2', '8 / span 2', '8 / span 2']}
+          text="GATHERING FOR AN ANNUAL FAMILY REUNION"
+        />
         <Content
           center={true}
-          column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-          row={['7 / span 3', '7 / span 3', '7 / span 3', '7 / span 3']}
+          column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
+          row={['10 / span 2', '10 / span 2', '10 / span 2', '10 / span 2']}
         >
-          <p>Ligonier conferences are held nationally and internationally, bringing trusted teachers to live events for growing Christians. We bring gifted teachers to local gatherings so that as many people as possible can join with us. We give thanks to God for the thousands who do so every year.</p>
-          <p>In addition to our popular biennial West Coast Conference, Ligonier hosts regional conferences in many states each year. Internationally, conferences have taken place in Germany, Northern Ireland, and Mexico. Future events are planned for the United Kingdom and other European countries, as well as Canada and Asia.</p>
+          <p>For over thirty years, our National Conference has been gathering thousands of Ligonier friends and supporters to hear trustworthy teaching from God’s Word. The conference is held over three days every spring in Orlando, Fla., and brings together Christians from many different backgrounds who make up the Ligonier family. Each year it is encouraging to see more firsttime attendees gathering from around the world. Your prayers and support enable our National Conference to serve as a great platform for equipping Christians with the truth.</p>
         </Content>
-      </Well>
-      <NumbersLarge
-        column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
-        description1='EVENTS IN A YEAR'
-        description2='NUMBER OF ATTENDEES'
-        number1='20+'
-        number2='12,000+'
-        row={['10 / span 2', '10 / span 2', '10 / span 2', '10 / span 2']}
-      />
-    </Well>
-  </Grid>
+
+        <Well size="l" top={true}>
+          <Backdrop />
+          <Subject
+            column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+            row={['1 / span 2', '1 / span 2', '1 / span 2', '1 / span 2']}
+          >
+            Regional &amp; International Conferences
+          </Subject>
+          <Divider
+            column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+            row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
+          />
+          <Heading
+            center={true}
+            column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+            row={['2 / span 3', '2 / span 3', '2 / span 3', '2 / span 3']}
+            text="GATHERING LOCALLY AND AROUND THE WORLD"
+          />
+          <Image
+            column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+            position='left center'
+            row={['4 / span 3', '4 / span 3', '4 / span 3', '4 / span 3']}
+            size='contain'
+            src={data.ConferenceSeattle.childImageSharp.fluid}
+            transform='translateY(1.5rem)'
+          />
+          <Well>
+            <Content
+              center={true}
+              column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+              row={['7 / span 3', '7 / span 3', '7 / span 3', '7 / span 3']}
+            >
+              <p>Ligonier conferences are held nationally and internationally, bringing trusted teachers to live events for growing Christians. We bring gifted teachers to local gatherings so that as many people as possible can join with us. We give thanks to God for the thousands who do so every year.</p>
+              <p>In addition to our popular biennial West Coast Conference, Ligonier hosts regional conferences in many states each year. Internationally, conferences have taken place in Germany, Northern Ireland, and Mexico. Future events are planned for the United Kingdom and other European countries, as well as Canada and Asia.</p>
+            </Content>
+          </Well>
+          <NumbersLarge
+            column={['10 / span 4', '10 / span 4', '10 / span 4', '10 / span 4']}
+            description1='EVENTS IN A YEAR'
+            description2='NUMBER OF ATTENDEES'
+            number1='20+'
+            number2='12,000+'
+            row={['10 / span 2', '10 / span 2', '10 / span 2', '10 / span 2']}
+          />
+        </Well>
+      </Grid>
+    )}
+  />
 )
 
 const Backdrop = styled('div')`
--- src/sections/gather-03.js
+++ src/sections/gather-03.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
 import Content from '../components/content'
@@ -9,124 +10,155 @@ import Image from '../components/image'
 import Subject from '../components/subject'
 import Well from '../components/well'
 
-import AskAnything from '../images/AskAnything.jpg'
-import Travel01 from '../images/Travel01.jpg'
-import Travel02 from '../images/Travel02.jpg'
-import Travel03 from '../images/Travel03.jpg'
-
 const IndexPage = () => (
-  <Grid name="gather03">
-    <Well bottom={true} size="l">
-      <Backdrop />
-      <Subject
-        column={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
-        row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
-      >
-        Study Cruises and Tours
-      </Subject>
-      <Divider
-        column={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
-        row={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
-      />
-      <Heading
-        center={true}
-        column={['2 / span 3', '2 / span 3', '2 / span 3', '2 / span 3']}
-        row={['3 / span 2', '3 / span 2', '3 / span 2', '3 / span 2']}
-        text="Gathering and Exploring Together"
-      />
-      <Image
-        column={['6 / span 3', '6 / span 3', '6 / span 3', '6 / span 3']}
-        position='center 30%'
-        row={['3 / span 3', '3 / span 3', '3 / span 3', '3 / span 3']}
-        src={Travel02}
-        transform='translateY(-2rem)'
-      />
-      <Well>
-        <Content
-          column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
-          row={['9 / span 4', '9 / span 4', '9 / span 4', '9 / span 4']}
+  <StaticQuery
+    query={graphql`
+      query Gather03Query {
+        AskAnything: file(relativePath: { eq: "AskAnything.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 700) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        Travel01: file(relativePath: { eq: "Travel01.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1200) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        Travel02: file(relativePath: { eq: "Travel02.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 700) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        Travel03: file(relativePath: { eq: "Travel03.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <Grid name="gather03">
+        <Well bottom={true} size="l">
+          <Backdrop />
+          <Subject
+            column={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
+            row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
+          >
+            Study Cruises and Tours
+          </Subject>
+          <Divider
+            column={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
+            row={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
+          />
+          <Heading
+            center={true}
+            column={['2 / span 3', '2 / span 3', '2 / span 3', '2 / span 3']}
+            row={['3 / span 2', '3 / span 2', '3 / span 2', '3 / span 2']}
+            text="Gathering and Exploring Together"
+          />
+          <Image
+            column={['6 / span 3', '6 / span 3', '6 / span 3', '6 / span 3']}
+            position='center 30%'
+            row={['3 / span 3', '3 / span 3', '3 / span 3', '3 / span 3']}
+            src={data.Travel02.childImageSharp.fluid}
+            transform='translateY(-2rem)'
+          />
+          <Well>
+            <Content
+              column={['2 / span 4', '2 / span 4', '2 / span 4', '2 / span 4']}
+              row={['9 / span 4', '9 / span 4', '9 / span 4', '9 / span 4']}
+            >
+              <p><strong>Ligonier Cruises</strong> gather ministry friends at sea for trustworthy teaching from God’s Word. Together they enjoy a memorable time with refreshing Christian fellowship set amid the glory of God’s creation. In addition to faithful teaching, Christians have the opportunity to meet like-minded believers. Popular destinations include Alaska, the Caribbean, and Europe.</p>
+              <p>From historic sites in Israel to Germany to the British Isles, Ligonier tours take inquisitive Christians to the places where God has worked in church history. Ligonier Teaching Fellows travel with attendees to guide, to teach, and to explain why our history still matters today. These non-donor funded tours provide a rare, first-hand opportunity for growing Christians to experience our family history together.</p>
+            </Content>
+          </Well>
+          <Image
+            column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
+            position='center 30%'
+            row={['5 / span 4', '5 / span 4', '5 / span 4', '5 / span 4']}
+            src={data.Travel01.childImageSharp.fluid}
+          />
+          <Well hide={true}>
+            <Image
+              column={['6 / span 2', '6 / span 2', '6 / span 2', '6 / span 2']}
+              position='center 30%'
+              row={['8 / span 2', '8 / span 2', '8 / span 2', '8 / span 2']}
+              src={data.Travel03.childImageSharp.fluid}
+              transform="translateX(3rem)"
+            />
+          </Well>
+        </Well>
+
+        <Subject
+          column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
+          name="institute-expository-preaching"
+          row={['6 / span 1', '6 / span 1', '6 / span 1', '6 / span 1']}
         >
-          <p><strong>Ligonier Cruises</strong> gather ministry friends at sea for trustworthy teaching from God’s Word. Together they enjoy a memorable time with refreshing Christian fellowship set amid the glory of God’s creation. In addition to faithful teaching, Christians have the opportunity to meet like-minded believers. Popular destinations include Alaska, the Caribbean, and Europe.</p>
-          <p>From historic sites in Israel to Germany to the British Isles, Ligonier tours take inquisitive Christians to the places where God has worked in church history. Ligonier Teaching Fellows travel with attendees to guide, to teach, and to explain why our history still matters today. These non-donor funded tours provide a rare, first-hand opportunity for growing Christians to experience our family history together.</p>
-        </Content>
-      </Well>
-      <Image
-        column={['2 / span 5', '2 / span 5', '2 / span 5', '2 / span 5']}
-        position='center 30%'
-        row={['5 / span 4', '5 / span 4', '5 / span 4', '5 / span 4']}
-        src={Travel01}
-      />
-      <Well hide={true}>
-        <Image
-          column={['6 / span 2', '6 / span 2', '6 / span 2', '6 / span 2']}
-          position='center 30%'
-          row={['8 / span 2', '8 / span 2', '8 / span 2', '8 / span 2']}
-          src={Travel03}
-          transform="translateX(3rem)"
+          THE INSTITUTE FOR EXPOSITORY PREACHING
+        </Subject>
+        <Divider
+          column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
+          row={['6 / span 2', '6 / span 2', '6 / span 2', '6 / span 2']}
         />
-      </Well>
-    </Well>
-
-    <Subject
-      column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
-      name="institute-expository-preaching"
-      row={['6 / span 1', '6 / span 1', '6 / span 1', '6 / span 1']}
-    >
-      THE INSTITUTE FOR EXPOSITORY PREACHING
-    </Subject>
-    <Divider
-      column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
-      row={['6 / span 2', '6 / span 2', '6 / span 2', '6 / span 2']}
-    />
-    <Heading
-      center={true}
-      column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
-      row={['7 / span 2', '7 / span 2', '7 / span 2', '7 / span 2']}
-      text="TRAINING BIBLICAL EXPOSITORS"
-    />
-    <Content
-      center={true}
-      column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
-      row={['9 / span 3', '9 / span 3', '9 / span 3', '9 / span 3']}
-    >
-      <p>The Institute for Expository Preaching trains pastors, church leaders, and Bible teachers in the art of expository preaching. With over forty-five years in preaching and teaching ministry, Dr. Steven Lawson leads these events across the country and around the world. Each event is dedicated to the passionate preaching and exposition of the Word of God so that the glorious gospel of Jesus Christ can be heard more clearly from pulpits everywhere.</p>
-    </Content>
-
-    <Well>
-      <Subject
-        column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
-        name="ask-anything"
-        row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
-      >
-        ASK ANYTHING AND TRUTH &amp; CONSEQUENCES
-      </Subject>
-      <Divider
-        column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
-        row={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
-      />
-      <Well>
-        <Image
-          column={['12 / span 3', '12 / span 3', '12 / span 3', '12 / span 3']}
-          position="center 90%"
+        <Heading
+          center={true}
+          column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
+          row={['7 / span 2', '7 / span 2', '7 / span 2', '7 / span 2']}
+          text="TRAINING BIBLICAL EXPOSITORS"
+        />
+        <Content
+          center={true}
+          column={['9 / span 2', '9 / span 2', '9 / span 2', '9 / span 2']}
           row={['9 / span 3', '9 / span 3', '9 / span 3', '9 / span 3']}
-          src={AskAnything}
-        />
-      </Well>
-      <Heading
-        center={true}
-        column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
-        row={['3 / span 2', '3 / span 2', '3 / span 2', '3 / span 2']}
-        text="CONTENDING FOR THE FAITH"
-      />
-      <Content
-        center={true}
-        column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
-        row={['5 / span 3', '5 / span 3', '5 / span 3', '5 / span 3']}
-      >
-        <p>New to major college campuses across the United States, Ask Anything events provide a platform for Dr. Albert Mohler to answer students’ questions live. Dr. Mohler equips Christian students to contend earnestly for the faith once delivered to the saints. He is joined by other Teaching Fellows for the companion Truth and Consequences training seminar, which helps Christian students debunk the tenets of secularism and trains them in a biblical worldview. By God’s grace, your financial support is making these events possible for students.</p>
-      </Content>
-    </Well>
-  </Grid>
+        >
+          <p>The Institute for Expository Preaching trains pastors, church leaders, and Bible teachers in the art of expository preaching. With over forty-five years in preaching and teaching ministry, Dr. Steven Lawson leads these events across the country and around the world. Each event is dedicated to the passionate preaching and exposition of the Word of God so that the glorious gospel of Jesus Christ can be heard more clearly from pulpits everywhere.</p>
+        </Content>
+
+        <Well>
+          <Subject
+            column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
+            name="ask-anything"
+            row={['2 / span 1', '2 / span 1', '2 / span 1', '2 / span 1']}
+          >
+            ASK ANYTHING AND TRUTH &amp; CONSEQUENCES
+          </Subject>
+          <Divider
+            column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
+            row={['2 / span 2', '2 / span 2', '2 / span 2', '2 / span 2']}
+          />
+          <Well>
+            <Image
+              column={['12 / span 3', '12 / span 3', '12 / span 3', '12 / span 3']}
+              position="center 90%"
+              row={['9 / span 3', '9 / span 3', '9 / span 3', '9 / span 3']}
+              src={data.AskAnything.childImageSharp.fluid}
+            />
+          </Well>
+          <Heading
+            center={true}
+            column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
+            row={['3 / span 2', '3 / span 2', '3 / span 2', '3 / span 2']}
+            text="CONTENDING FOR THE FAITH"
+          />
+          <Content
+            center={true}
+            column={['12 / span 2', '12 / span 2', '12 / span 2', '12 / span 2']}
+            row={['5 / span 3', '5 / span 3', '5 / span 3', '5 / span 3']}
+          >
+            <p>New to major college campuses across the United States, Ask Anything events provide a platform for Dr. Albert Mohler to answer students’ questions live. Dr. Mohler equips Christian students to contend earnestly for the faith once delivered to the saints. He is joined by other Teaching Fellows for the companion Truth and Consequences training seminar, which helps Christian students debunk the tenets of secularism and trains them in a biblical worldview. By God’s grace, your financial support is making these events possible for students.</p>
+          </Content>
+        </Well>
+      </Grid>
+    )}
+  />
 )
 
 const Backdrop = styled('div')`
